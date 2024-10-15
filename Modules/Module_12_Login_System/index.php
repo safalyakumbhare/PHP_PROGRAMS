d770cdb26c2596e533df4b6ff41ecf8d6809cc6d
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-10-15 22:23:52 +0530

    Login Task Frontend Ready

---
+++ Modules/Module_12_Login_System/index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<style>
+    .container-fluid {
+        height: 100vh;
+        background-color: #f8f9fa;
+    }
+</style>
+
+<body>
+
+    <div class="container-fluid bg-secondary">
+        <h1 class="text-center pb-md-5 pt-md-5">Login!</h1>
+
+        <div class="row justify-content-evenly">
+            <div class="col-md-4 bg-light p-md-4 rounded-4">
+                <form action="login.php" method="POST">
+                    <div class="row justify-content-center">
+                        <div class="col-md-12">
+                            <div class="form-group mb-3">
+                                <label for="username" class="form-label">Username : </label>
+                                <input type="text" class="form-control" id="username" name="username" required>
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div class="form-group mb-3">
+                                <label for="password" class="form-label">Password : </label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                        </div>
+
+                        <div class="col-md-3">
+                            <button type="submit" class="w-100 btn btn-primary">Login</button>
+                        </div>
+                        <div class="w-100"></div>
+                        <div class="col-md-6 mt-md-2 p-md-3 text-center border-end border-1">
+                            <small>Not Registered? <a href="register.php">Register Now</a></small>
+                        </div>
+                        <div class="col-md-6 mt-md-2 p-md-3 text-center">
+                            <small><a href="register.php" >Forget Password</a></small>
+                        </div>
+
+                    </div>
+            </div>
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+
+</html>
---
+++ Modules/Module_12_Login_System/welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <h1 class="text-center mt-5">Welcome, User!</h1>
+    <a href="logout.php" class="btn btn-danger mt-5">Logout</a>
+
+</body>
+
+</html>
